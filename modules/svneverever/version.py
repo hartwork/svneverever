330b58e4cf00b151e5582b99c44ee7017bf2ea10
Sebastian Pipping <sebastian@pipping.org>
2013-05-11 03:02:07 +0200

    Bump version to 1.2.2

--- modules/svneverever/version.py
+++ modules/svneverever/version.py
@@ -1,5 +1,5 @@
 # Copyright (C) 2010-2011 Sebastian Pipping <sebastian@pipping.org>
 # Licensed under GPL v3 or later
 
-VERSION = (1, 2, 1)
+VERSION = (1, 2, 2)
 VERSION_STR = '.'.join(str(e) for e in VERSION)
