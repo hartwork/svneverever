44de54ec586f81ec107399805150e95239f3abbc
Sebastian Pipping <sebastian@pipping.org>
2021-07-03 12:49:07 +0200

    Bump version to 1.6.0

--- svneverever/version.py
+++ svneverever/version.py
@@ -1,5 +1,5 @@
-# Copyright (C) 2010-2019 Sebastian Pipping <sebastian@pipping.org>
+# Copyright (C) 2010-2021 Sebastian Pipping <sebastian@pipping.org>
 # Licensed under GPL v3 or later
 
-VERSION = (1, 5, 0)
+VERSION = (1, 6, 0)
 VERSION_STR = '.'.join(str(e) for e in VERSION)
