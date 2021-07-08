bf72dae9d4eadb18c2fa8c1bbe2c245527919ec2
Sebastian Pipping <sebastian@pipping.org>
2021-07-09 00:50:45 +0200

    Bump version to 1.7.1

--- svneverever/version.py
+++ svneverever/version.py
@@ -1,5 +1,5 @@
 # Copyright (C) 2010-2021 Sebastian Pipping <sebastian@pipping.org>
 # Licensed under GPL v3 or later
 
-VERSION = (1, 7, 0)
+VERSION = (1, 7, 1)
 VERSION_STR = '.'.join(str(e) for e in VERSION)
